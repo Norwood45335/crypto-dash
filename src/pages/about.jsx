5f12cacdd96766e2176dd77a7d96d185b241d850
rjnorwood1@icloud.com <150552474+Norwood45335@users.noreply.github.com>
2025-08-02 16:15:21 -0400

    Initial commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+    return ( 
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ducimus pariatur eaque laudantium, ea impedit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sit suscipit corporis. Sunt, porro dicta similique quam consectetur quidem quo?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt voluptates voluptatibus, repellat, quasi officia unde excepturi quam adipisci possimus saepe tempora, neque molestias voluptatem placeat!</p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
